54fcbfa2070712a692c86ee4dab24fa02384450d
Arman Salehi <arman.salehi@student.rocva.nl>
2021-02-02 10:31:59 +0100

    antwoorden sql

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ('Henk', 'Joosten', '1999-01-22', 'Amsterdam');
 
+INSERT INTO studenten (studenten_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('Joost', 'van Ijzer', '1999-01-22', 'Amsterdam');
+
+INSERT INTO studenten (studenten_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('Moos', 'de Vries', '1999-01-22', 'Amsterdam');
