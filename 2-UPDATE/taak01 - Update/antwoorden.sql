e3178b09835a8919c7e41dd84169a0bf6b6224d4
Arman Salehi <arman.salehi@student.rocva.nl>
2021-02-02 11:36:54 +0100

    antwoorden sql

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -1,9 +1,20 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET woonplaats = 'Amstelveen';
 -- Opdracht 2 
 
 -- Opdracht 3
-
+UPDATE studenten2 
+SET geboortedatum = '2000-12-12';
 -- Opdracht 4
+UPDATE studenten2
+SET klas = '9C'
+WHERE voornaam = 'Mohammed';
 
+UPDATE studenten2
+SET klas = '9A'
+WHERE voornaam = 'Jan';
 -- Opdracht 5
+UPDATE studenten2
+SET voornaam = 'Johannah'
+WHERE student_id = '6';
